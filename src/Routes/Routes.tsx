a23dcf5913fc6498b0d7d627cfcf301694ef5f6b
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-09-18 11:05:11 +0500

    added Lazy Compnoent importing

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,19 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "../Pages/Home";
-import TaskPage from "../Pages/Task";
-import AddTaskPage from "../Pages/AddTask";
-import EditPage from "../Pages/Update";
+import { lazy, Suspense } from "react";
 
-function RoutesComp(): any {
+const HomePage = lazy(() => import("../Pages/Home"));
+const TaskPage = lazy(() => import("../Pages/Task"));
+const AddTaskPage = lazy(() => import("../Pages/AddTask"));
+const EditPage = lazy(() => import("../Pages/Update"));
+
+function RoutesComp(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/tasks" element={<TaskPage />} />
-        <Route path="/add" element={<AddTaskPage />} />
-        <Route path="/update/:id" element={<EditPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-screen">
+          Loading...
+        </div>
+      }
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tasks" element={<TaskPage />} />
+          <Route path="/add" element={<AddTaskPage />} />
+          <Route path="/update/:id" element={<EditPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
+
 export default RoutesComp;
